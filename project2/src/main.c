9b5c90d34297c9c4b5023bdf9b3ea074214ff312
1cekrim <1cekrim.dev@gmail.com>
2020-10-01 20:27:22 +0900

    코드 포맷팅

--- project2/src/main.c
+++ project2/src/main.c
@@ -2,11 +2,11 @@
 
 // MAIN
 
-int main( int argc, char ** argv ) {
-
-    char * input_file;
-    FILE * fp;
-    node * root;
+int main(int argc, char** argv)
+{
+    char* input_file;
+    FILE* fp;
+    node* root;
     int input, range2;
     char instruction;
     char license_part;
@@ -14,9 +14,11 @@ int main( int argc, char ** argv ) {
     root = NULL;
     verbose_output = false;
 
-    if (argc > 1) {
+    if (argc > 1)
+    {
         order = atoi(argv[1]);
-        if (order < MIN_ORDER || order > MAX_ORDER) {
+        if (order < MIN_ORDER || order > MAX_ORDER)
+        {
             fprintf(stderr, "Invalid order: %d .\n\n", order);
             usage_3();
             exit(EXIT_FAILURE);
@@ -24,17 +26,20 @@ int main( int argc, char ** argv ) {
     }
 
     license_notice();
-    usage_1();  
+    usage_1();
     usage_2();
 
-    if (argc > 2) {
+    if (argc > 2)
+    {
         input_file = argv[2];
         fp = fopen(input_file, "r");
-        if (fp == NULL) {
+        if (fp == NULL)
+        {
             perror("Failure  open input file.");
             exit(EXIT_FAILURE);
         }
-        while (!feof(fp)) {
+        while (!feof(fp))
+        {
             fscanf(fp, "%d\n", &input);
             root = insert(root, input, input);
         }
@@ -43,55 +48,60 @@ int main( int argc, char ** argv ) {
     }
 
     printf("> ");
-    while (scanf("%c", &instruction) != EOF) {
-        switch (instruction) {
-        case 'd':
-            scanf("%d", &input);
-            root = delete(root, input);
-            print_tree(root);
-            break;
-        case 'i':
-            scanf("%d", &input);
-            root = insert(root, input, input);
-            print_tree(root);
-            break;
-        case 'f':
-        case 'p':
-            scanf("%d", &input);
-            find_and_print(root, input, instruction == 'p');
-            break;
-        case 'r':
-            scanf("%d %d", &input, &range2);
-            if (input > range2) {
-                int tmp = range2;
-                range2 = input;
-                input = tmp;
-            }
-            find_and_print_range(root, input, range2, instruction == 'p');
-            break;
-        case 'l':
-            print_leaves(root);
-            break;
-        case 'q':
-            while (getchar() != (int)'\n');
-            return EXIT_SUCCESS;
-            break;
-        case 't':
-            print_tree(root);
-            break;
-        case 'v':
-            verbose_output = !verbose_output;
-            break;
-        case 'x':
-            if (root)
-                root = destroy_tree(root);
-            print_tree(root);
-            break;
-        default:
-            usage_2();
-            break;
+    while (scanf("%c", &instruction) != EOF)
+    {
+        switch (instruction)
+        {
+            case 'd':
+                scanf("%d", &input);
+                root = delete (root, input);
+                print_tree(root);
+                break;
+            case 'i':
+                scanf("%d", &input);
+                root = insert(root, input, input);
+                print_tree(root);
+                break;
+            case 'f':
+            case 'p':
+                scanf("%d", &input);
+                find_and_print(root, input, instruction == 'p');
+                break;
+            case 'r':
+                scanf("%d %d", &input, &range2);
+                if (input > range2)
+                {
+                    int tmp = range2;
+                    range2 = input;
+                    input = tmp;
+                }
+                find_and_print_range(root, input, range2, instruction == 'p');
+                break;
+            case 'l':
+                print_leaves(root);
+                break;
+            case 'q':
+                while (getchar() != (int)'\n')
+                    ;
+                return EXIT_SUCCESS;
+                break;
+            case 't':
+                print_tree(root);
+                break;
+            case 'v':
+                verbose_output = !verbose_output;
+                break;
+            case 'x':
+                if (root)
+                    root = destroy_tree(root);
+                print_tree(root);
+                break;
+            default:
+                usage_2();
+                break;
         }
-        while (getchar() != (int)'\n');
+        while (getchar() != (int)'\n')
+            ;
         printf("> ");
     }
     printf("\n");
