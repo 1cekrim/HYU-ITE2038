5b8339856a345f8c8c2f245b0ff2fa25d023e7f2
1cekrim <1cekrim.dev@gmail.com>
2020-10-08 11:51:47 +0900

    주석과 필요없는 함수 몇개 제거

--- project2/src/bpt.cc
+++ project2/src/bpt.cc
@@ -2,55 +2,6 @@
  *  bpt.c
  */
 #define Version "1.14"
-/*
- *
- *  bpt:  B+ Tree Implementation
- *  Copyright (C) 2010-2016  Amittai Aviram  http://www.amittai.com
- *  All rights reserved.
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- *  1. Redistributions of source code must retain the above copyright notice,
- *  this list of conditions and the following disclaimer.
- *
- *  2. Redistributions in binary form must reproduce the above copyright notice,
- *  this list of conditions and the following disclaimer in the documentation
- *  and/or other materials provided with the distribution.
-
- *  3. Neither the name of the copyright holder nor the names of its
- *  contributors may be used to endorse or promote products derived from this
- *  software without specific prior written permission.
-
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
-
- *  Author:  Amittai Aviram
- *    http://www.amittai.com
- *    amittai.aviram@gmail.edu or afa13@columbia.edu
- *  Original Date:  26 June 2010
- *  Last modified: 17 June 2016
- *
- *  This implementation demonstrates the B+ tree data structure
- *  for educational purposes, includin insertion, deletion, search, and display
- *  of the search path, the leaves, or the whole tree.
- *
- *  Must be compiled with a C99-compliant C compiler such as the latest GCC.
- *
- *  Usage:  bpt [order]
- *  where order is an optional argument
- *  (integer MIN_ORDER <= order <= MAX_ORDER)
- *  defined as the maximal number of pointers in any node.
- *
- */
 
 #include "bpt.hpp"
 
@@ -82,112 +33,6 @@ node* queue = NULL;
  */
 bool verbose_output = false;
 
-// FUNCTION DEFINITIONS.
-
-// OUTPUT AND UTILITIES
-
-/* Copyright and license notice to user at startup.
- */
-void license_notice(void)
-{
-    printf(
-        "bpt version %s -- Copyright (C) 2010  Amittai Aviram "
-        "http://www.amittai.com\n",
-        Version);
-    printf(
-        "This program comes with ABSOLUTELY NO WARRANTY; for details "
-        "type `show w'.\n"
-        "This is free software, and you are welcome to redistribute it\n"
-        "under certain conditions; type `show c' for details.\n\n");
-}
-
-/* Routine to print portion of GPL license to stdout.
- */
-void print_license(int license_part)
-{
-    int start, end, line;
-    FILE* fp;
-    char buffer[0x100];
-
-    switch (license_part)
-    {
-        case LICENSE_WARRANTEE:
-            start = LICENSE_WARRANTEE_START;
-            end = LICENSE_WARRANTEE_END;
-            break;
-        case LICENSE_CONDITIONS:
-            start = LICENSE_CONDITIONS_START;
-            end = LICENSE_CONDITIONS_END;
-            break;
-        default:
-            return;
-    }
-
-    fp = fopen(LICENSE_FILE, "r");
-    if (fp == NULL)
-    {
-        perror("print_license: fopen");
-        exit(EXIT_FAILURE);
-    }
-    for (line = 0; line < start; line++)
-        fgets(buffer, sizeof(buffer), fp);
-    for (; line < end; line++)
-    {
-        fgets(buffer, sizeof(buffer), fp);
-        printf("%s", buffer);
-    }
-    fclose(fp);
-}
-
-/* First message to the user.
- */
-void usage_1(void)
-{
-    printf("B+ Tree of Order %d.\n", order);
-    printf(
-        "Following Silberschatz, Korth, Sidarshan, Database Concepts, "
-        "5th ed.\n\n"
-        "To build a B+ tree of a different order, start again and enter "
-        "the order\n"
-        "as an integer argument:  bpt <order>  ");
-    printf("(%d <= order <= %d).\n", MIN_ORDER, MAX_ORDER);
-    printf(
-        "To start with input from a file of newline-delimited integers, \n"
-        "start again and enter the order followed by the filename:\n"
-        "bpt <order> <inputfile> .\n");
-}
-
-/* Second message to the user.
- */
-void usage_2(void)
-{
-    printf(
-        "Enter any of the following commands after the prompt > :\n"
-        "\ti <k>  -- Insert <k> (an integer) as both key and value).\n"
-        "\tf <k>  -- Find the value under key <k>.\n"
-        "\tp <k> -- Print the path from the root to key k and its associated "
-        "value.\n"
-        "\tr <k1> <k2> -- Print the keys and values found in the range "
-        "[<k1>, <k2>\n"
-        "\td <k>  -- Delete key <k> and its associated value.\n"
-        "\tx -- Destroy the whole tree.  Start again with an empty tree of the "
-        "same order.\n"
-        "\tt -- Print the B+ tree.\n"
-        "\tl -- Print the keys of the leaves (bottom row of the tree).\n"
-        "\tv -- Toggle output of pointer addresses (\"verbose\") in tree and "
-        "leaves.\n"
-        "\tq -- Quit. (Or use Ctl-D.)\n"
-        "\t? -- Print this help message.\n");
-}
-
-/* Brief usage note.
- */
-void usage_3(void)
-{
-    printf("Usage: ./bpt [<order>]\n");
-    printf("\twhere %d <= order <= %d .\n", MIN_ORDER, MAX_ORDER);
-}
-
 /* Helper function for printing the
  * tree out.  See print_tree.
  */
@@ -204,7 +49,7 @@ void enqueue(node* new_node)
         c = queue;
         while (c->next != NULL)
         {
-            c = c->next;
+            c = c->next;sdfa
         }
         c->next = new_node;
         new_node->next = NULL;
