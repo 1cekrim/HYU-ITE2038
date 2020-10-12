b5b85b4835aba1882990c5589464be36666aed65
1cekrim <1cekrim.dev@gmail.com>
2020-10-13 02:14:53 +0900

    10000개 find 잘 되는거 확인

--- project2/main_src/main.c
+++ project2/main_src/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 #include "dbms.h"
-#include "file.h"
+#include "dbms.h"
 
 int main()
 {
-    
+    open_table("sample_10000.db");
+    for (int i = 1; i < 10000; ++i)
+    {
+        char data[120];
+        db_find(i, data);
+        puts(data);
+    }
     return 0;
 }
