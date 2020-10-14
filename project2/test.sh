9e584bdb673b9ad69ad73feaabbc610cb8aeb02e
1cekrim <1cekrim.dev@gmail.com>
2020-10-15 01:39:24 +0900

    성능 체크

--- project2/test.sh
+++ project2/test.sh
@@ -3,7 +3,7 @@ cd builds
 cp test /mnt/tmpfs/test
 cd /mnt/tmpfs
 ./test
-rm /mnt/tmpfs/*.db
-rm /mnt/tmpfs/test
-cd ~/github/2020_ite2038_2019027192/project2
-make clean
+# rm /mnt/tmpfs/*.db
+# rm /mnt/tmpfs/test
+# cd ~/github/2020_ite2038_2019027192/project2
+# make clean
