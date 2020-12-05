b513d3d3182043f7b1a8075f6f12cef392723cbf
1cekrim <1cekrim.dev@gmail.com>
2020-12-05 18:06:38 +0900

    main.sh

--- project5/main.sh
+++ project5/main.sh
@@ -5,9 +5,10 @@ rm /mnt/tmpfs/main
 cd builds
 cp main /mnt/tmpfs/main
 cd /mnt/tmpfs
-./main deadlock_test
-cp insert.db ~/github/2020_ite2038_2019027192/project2/insert.db
+# valgrind ./main slock_test >> result.txt
+./main slock_test
+# cp result.txt ~/github/2020_ite2038_2019027192/project5/result.txt
 rm /mnt/tmpfs/*.db
 rm /mnt/tmpfs/main
-cd ~/github/2020_ite2038_2019027192/project2
+cd ~/github/2020_ite2038_2019027192/project5
 make clean
