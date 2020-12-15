0350e43a4c2d2c46f9a3c9096619cc51c625bc4a
1cekrim <1cekrim.dev@gmail.com>
2020-12-15 15:37:07 +0900

    출력 추가

--- project6/src/table_manager.cc
+++ project6/src/table_manager.cc
@@ -9,8 +9,11 @@ bool TableManager::init_db(int buf_num, int flag, int log_num, char* log_path, c
     CHECK_WITH_LOG(BufferController::instance().init_buffer(buf_num), false,
                    "init buffer failure");
     valid_table_manager = true;
+    std::cout << "init_db open\n";
     LogManager::instance().open(log_path, logmsg);
+    std::cout << "init_db recovery\n";
     LogManager::instance().recovery(RecoveryMode(flag), log_num);
+    std::cout << "init_db end\n";
     return true;
 }
 
